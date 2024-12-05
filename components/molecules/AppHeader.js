6cc7ea2a9a822cf763b40f83e892e4f9b05515dd
Andreas Pinheiro <andreaspinheiroprincipal@gmail.com>
2024-12-05 13:50:43 -0300

    header 98% done

--- components/molecules/AppHeader.js
+++ components/molecules/AppHeader.js
@@ -1,35 +1,50 @@
-// imports
+// imports:
 import styled from "styled-components/native";
+import Container from '../atoms/Container'
 
 // assets:
-const ImgNutricia = '../../assets/nutricia-logo.png'
+const NutriciaImg = '../../assets/img/nutricia-logo.png'
+const MilnutriImg = '../../assets/img/milnutri-logo.png'
 
-// component function
+// component function:
 export default function AppHeader(){
     return(
         <HeaderWrapper>
-            {/* nutricia logo */}
-            <NutriciaLogoContainer>
-                <NutriciaLogoImage source={require(ImgNutricia)}/>
-            </NutriciaLogoContainer>
+            {/* nutricia logo & container */}
+            <Container 
+                height={'90px'}
+                padding={'47px 0 10px 0'} 
+                justify={'center'} 
+                align={'center'} 
+                bgcolor={'#ddf7ff'}
+            >
+                <NutriciaLogoImage source={require( NutriciaImg )}/>
+            </Container>
+
+            {/* milnutri logo & container */}
+            <Container
+                padding={'12px 0 12px 10px'}
+                bgcolor={'#c0f1ff'}
+            >
+                <MilnutriLogoImage source={require( MilnutriImg )}/>
+            </Container>
         </HeaderWrapper>
     )
 }
 
-// component styles
+// component styles:
+    // wrapper
 const HeaderWrapper = styled.View`
     width: 100%;
     gap: 5px;
 `
 
-const NutriciaLogoContainer = styled.View`
-    width: 100%;
-    height: 45px;
-    background-color: #ddf7ff;
-    justify-content: center;
-    align-items: center;
-`
+    // images
 const NutriciaLogoImage = styled.Image`
     width: 110px;
     height: 45px;
+`
+const MilnutriLogoImage = styled.Image`
+    width: 120px;
+    height: 54px;
 `
